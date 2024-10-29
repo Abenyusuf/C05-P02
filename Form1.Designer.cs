d5f41dda1009ecd0e2a2a5547be8a5fde6ab8041
abeny <abeny@AHMEDBENYUSUF>
2024-10-29 11:58:46 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace C05_P02
+{
+    partial class DiceRoller
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(DiceRoller));
+            this.DiceOnePictureBoxA = new System.Windows.Forms.PictureBox();
+            this.DiceSixPictureBoxB = new System.Windows.Forms.PictureBox();
+            this.DiceFivePictureBoxA = new System.Windows.Forms.PictureBox();
+            this.DiceFourPictureBoxA = new System.Windows.Forms.PictureBox();
+            this.DiceThreePictureBoxA = new System.Windows.Forms.PictureBox();
+            this.DiceTwoPictureBoxA = new System.Windows.Forms.PictureBox();
+            this.DiceTwoPictureBoxB = new System.Windows.Forms.PictureBox();
+            this.DiceThreePictureBoxB = new System.Windows.Forms.PictureBox();
+            this.DiceFourPictureBoxB = new System.Windows.Forms.PictureBox();
+            this.DiceFivePictureBoxB = new System.Windows.Forms.PictureBox();
+            this.DiceSixPictureBoxA = new System.Windows.Forms.PictureBox();
+            this.DiceOnePictureBoxB = new System.Windows.Forms.PictureBox();
+            this.RollButton = new System.Windows.Forms.Button();
+            this.ExitButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceOnePictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceSixPictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFivePictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFourPictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceThreePictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceTwoPictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceTwoPictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceThreePictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFourPictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFivePictureBoxB)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceSixPictureBoxA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceOnePictureBoxB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DiceOnePictureBoxA
+            // 
+            this.DiceOnePictureBoxA.Image = ((System.Drawing.Image)(resources.GetObject("DiceOnePictureBoxA.Image")));
+            this.DiceOnePictureBoxA.Location = new System.Drawing.Point(41, 12);
+            this.DiceOnePictureBoxA.Name = "DiceOnePictureBoxA";
+            this.DiceOnePictureBoxA.Size = new System.Drawing.Size(104, 104);
+            this.DiceOnePictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceOnePictureBoxA.TabIndex = 0;
+            this.DiceOnePictureBoxA.TabStop = false;
+            // 
+            // DiceSixPictureBoxB
+            // 
+            this.DiceSixPictureBoxB.Image = ((System.Drawing.Image)(resources.GetObject("DiceSixPictureBoxB.Image")));
+            this.DiceSixPictureBoxB.Location = new System.Drawing.Point(291, 12);
+            this.DiceSixPictureBoxB.Name = "DiceSixPictureBoxB";
+            this.DiceSixPictureBoxB.Size = new System.Drawing.Size(104, 104);
+            this.DiceSixPictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceSixPictureBoxB.TabIndex = 1;
+            this.DiceSixPictureBoxB.TabStop = false;
+            // 
+            // DiceFivePictureBoxA
+            // 
+            this.DiceFivePictureBoxA.Image = ((System.Drawing.Image)(resources.GetObject("DiceFivePictureBoxA.Image")));
+            this.DiceFivePictureBoxA.Location = new System.Drawing.Point(41, 12);
+            this.DiceFivePictureBoxA.Name = "DiceFivePictureBoxA";
+            this.DiceFivePictureBoxA.Size = new System.Drawing.Size(104, 104);
+            this.DiceFivePictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceFivePictureBoxA.TabIndex = 2;
+            this.DiceFivePictureBoxA.TabStop = false;
+            // 
+            // DiceFourPictureBoxA
+            // 
+            this.DiceFourPictureBoxA.Image = ((System.Drawing.Image)(resources.GetObject("DiceFourPictureBoxA.Image")));
+            this.DiceFourPictureBoxA.Location = new System.Drawing.Point(41, 12);
+            this.DiceFourPictureBoxA.Name = "DiceFourPictureBoxA";
+            this.DiceFourPictureBoxA.Size = new System.Drawing.Size(104, 104);
+            this.DiceFourPictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceFourPictureBoxA.TabIndex = 3;
+            this.DiceFourPictureBoxA.TabStop = false;
+            // 
+            // DiceThreePictureBoxA
+            // 
+            this.DiceThreePictureBoxA.Image = ((System.Drawing.Image)(resources.GetObject("DiceThreePictureBoxA.Image")));
+            this.DiceThreePictureBoxA.Location = new System.Drawing.Point(41, 12);
+            this.DiceThreePictureBoxA.Name = "DiceThreePictureBoxA";
+            this.DiceThreePictureBoxA.Size = new System.Drawing.Size(104, 104);
+            this.DiceThreePictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceThreePictureBoxA.TabIndex = 4;
+            this.DiceThreePictureBoxA.TabStop = false;
+            // 
+            // DiceTwoPictureBoxA
+            // 
+            this.DiceTwoPictureBoxA.Image = ((System.Drawing.Image)(resources.GetObject("DiceTwoPictureBoxA.Image")));
+            this.DiceTwoPictureBoxA.Location = new System.Drawing.Point(41, 12);
+            this.DiceTwoPictureBoxA.Name = "DiceTwoPictureBoxA";
+            this.DiceTwoPictureBoxA.Size = new System.Drawing.Size(104, 104);
+            this.DiceTwoPictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceTwoPictureBoxA.TabIndex = 5;
+            this.DiceTwoPictureBoxA.TabStop = false;
+            // 
+            // DiceTwoPictureBoxB
+            // 
+            this.DiceTwoPictureBoxB.Image = ((System.Drawing.Image)(resources.GetObject("DiceTwoPictureBoxB.Image")));
+            this.DiceTwoPictureBoxB.Location = new System.Drawing.Point(291, 12);
+            this.DiceTwoPictureBoxB.Name = "DiceTwoPictureBoxB";
+            this.DiceTwoPictureBoxB.Size = new System.Drawing.Size(104, 104);
+            this.DiceTwoPictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceTwoPictureBoxB.TabIndex = 11;
+            this.DiceTwoPictureBoxB.TabStop = false;
+            // 
+            // DiceThreePictureBoxB
+            // 
+            this.DiceThreePictureBoxB.Image = ((System.Drawing.Image)(resources.GetObject("DiceThreePictureBoxB.Image")));
+            this.DiceThreePictureBoxB.Location = new System.Drawing.Point(291, 12);
+            this.DiceThreePictureBoxB.Name = "DiceThreePictureBoxB";
+            this.DiceThreePictureBoxB.Size = new System.Drawing.Size(104, 104);
+            this.DiceThreePictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceThreePictureBoxB.TabIndex = 10;
+            this.DiceThreePictureBoxB.TabStop = false;
+            // 
+            // DiceFourPictureBoxB
+            // 
+            this.DiceFourPictureBoxB.Image = ((System.Drawing.Image)(resources.GetObject("DiceFourPictureBoxB.Image")));
+            this.DiceFourPictureBoxB.Location = new System.Drawing.Point(291, 12);
+            this.DiceFourPictureBoxB.Name = "DiceFourPictureBoxB";
+            this.DiceFourPictureBoxB.Size = new System.Drawing.Size(104, 104);
+            this.DiceFourPictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceFourPictureBoxB.TabIndex = 9;
+            this.DiceFourPictureBoxB.TabStop = false;
+            // 
+            // DiceFivePictureBoxB
+            // 
+            this.DiceFivePictureBoxB.Image = ((System.Drawing.Image)(resources.GetObject("DiceFivePictureBoxB.Image")));
+            this.DiceFivePictureBoxB.Location = new System.Drawing.Point(291, 12);
+            this.DiceFivePictureBoxB.Name = "DiceFivePictureBoxB";
+            this.DiceFivePictureBoxB.Size = new System.Drawing.Size(104, 104);
+            this.DiceFivePictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceFivePictureBoxB.TabIndex = 8;
+            this.DiceFivePictureBoxB.TabStop = false;
+            // 
+            // DiceSixPictureBoxA
+            // 
+            this.DiceSixPictureBoxA.Image = ((System.Drawing.Image)(resources.GetObject("DiceSixPictureBoxA.Image")));
+            this.DiceSixPictureBoxA.Location = new System.Drawing.Point(41, 12);
+            this.DiceSixPictureBoxA.Name = "DiceSixPictureBoxA";
+            this.DiceSixPictureBoxA.Size = new System.Drawing.Size(104, 104);
+            this.DiceSixPictureBoxA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceSixPictureBoxA.TabIndex = 7;
+            this.DiceSixPictureBoxA.TabStop = false;
+            // 
+            // DiceOnePictureBoxB
+            // 
+            this.DiceOnePictureBoxB.Image = ((System.Drawing.Image)(resources.GetObject("DiceOnePictureBoxB.Image")));
+            this.DiceOnePictureBoxB.Location = new System.Drawing.Point(291, 12);
+            this.DiceOnePictureBoxB.Name = "DiceOnePictureBoxB";
+            this.DiceOnePictureBoxB.Size = new System.Drawing.Size(104, 104);
+            this.DiceOnePictureBoxB.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.DiceOnePictureBoxB.TabIndex = 6;
+            this.DiceOnePictureBoxB.TabStop = false;
+            // 
+            // RollButton
+            // 
+            this.RollButton.Location = new System.Drawing.Point(70, 215);
+            this.RollButton.Name = "RollButton";
+            this.RollButton.Size = new System.Drawing.Size(75, 35);
+            this.RollButton.TabIndex = 12;
+            this.RollButton.Text = "Roll";
+            this.RollButton.UseVisualStyleBackColor = true;
+            this.RollButton.Click += new System.EventHandler(this.RollButton_Click);
+            // 
+            // ExitButton
+            // 
+            this.ExitButton.Location = new System.Drawing.Point(291, 215);
+            this.ExitButton.Name = "ExitButton";
+            this.ExitButton.Size = new System.Drawing.Size(75, 35);
+            this.ExitButton.TabIndex = 13;
+            this.ExitButton.Text = "Exit";
+            this.ExitButton.UseVisualStyleBackColor = true;
+            this.ExitButton.Click += new System.EventHandler(this.ExitButton_Click);
+            // 
+            // DiceRoller
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(492, 334);
+            this.Controls.Add(this.ExitButton);
+            this.Controls.Add(this.RollButton);
+            this.Controls.Add(this.DiceTwoPictureBoxB);
+            this.Controls.Add(this.DiceThreePictureBoxB);
+            this.Controls.Add(this.DiceFourPictureBoxB);
+            this.Controls.Add(this.DiceFivePictureBoxB);
+            this.Controls.Add(this.DiceSixPictureBoxA);
+            this.Controls.Add(this.DiceOnePictureBoxB);
+            this.Controls.Add(this.DiceTwoPictureBoxA);
+            this.Controls.Add(this.DiceThreePictureBoxA);
+            this.Controls.Add(this.DiceFourPictureBoxA);
+            this.Controls.Add(this.DiceFivePictureBoxA);
+            this.Controls.Add(this.DiceSixPictureBoxB);
+            this.Controls.Add(this.DiceOnePictureBoxA);
+            this.Name = "DiceRoller";
+            this.Text = "Dice Roller";
+            ((System.ComponentModel.ISupportInitialize)(this.DiceOnePictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceSixPictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFivePictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFourPictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceThreePictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceTwoPictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceTwoPictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceThreePictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFourPictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceFivePictureBoxB)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceSixPictureBoxA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.DiceOnePictureBoxB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox DiceOnePictureBoxA;
+        private System.Windows.Forms.PictureBox DiceSixPictureBoxB;
+        private System.Windows.Forms.PictureBox DiceFivePictureBoxA;
+        private System.Windows.Forms.PictureBox DiceFourPictureBoxA;
+        private System.Windows.Forms.PictureBox DiceThreePictureBoxA;
+        private System.Windows.Forms.PictureBox DiceTwoPictureBoxA;
+        private System.Windows.Forms.PictureBox DiceTwoPictureBoxB;
+        private System.Windows.Forms.PictureBox DiceThreePictureBoxB;
+        private System.Windows.Forms.PictureBox DiceFourPictureBoxB;
+        private System.Windows.Forms.PictureBox DiceFivePictureBoxB;
+        private System.Windows.Forms.PictureBox DiceSixPictureBoxA;
+        private System.Windows.Forms.PictureBox DiceOnePictureBoxB;
+        private System.Windows.Forms.Button RollButton;
+        private System.Windows.Forms.Button ExitButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,129 @@
+﻿//Name : Ahmed Benyusuf
+//Class and Section (CS 313 01)
+//Assignment (Program 02 Chapter 05)
+//Description of the Program : This program is a dice roller that generates two random numbers between 1 and 6 and displays the corresponding dice images for each number.
+
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C05_P02
+{
+    public partial class DiceRoller : Form
+    {
+        public DiceRoller()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void RollButton_Click(object sender, EventArgs e)
+        {
+
+            //Hide all of the possible dice images
+                DiceOnePictureBoxA.Visible = false;
+                DiceOnePictureBoxB.Visible = false;
+
+                DiceTwoPictureBoxA.Visible = false;
+                DiceTwoPictureBoxB.Visible = false;
+
+                DiceThreePictureBoxA.Visible = false;
+                DiceThreePictureBoxB.Visible = false;
+
+                DiceFourPictureBoxA.Visible = false;
+                DiceFourPictureBoxB.Visible = false;
+
+                DiceFivePictureBoxA.Visible = false;
+                DiceFivePictureBoxB.Visible = false;
+
+            
+                DiceSixPictureBoxA.Visible = false;
+                DiceSixPictureBoxB.Visible = false;
+
+            //Create a random number generator
+            Random rnd = new Random();
+            //Generate two random numbers between 1 and 6
+            int DiceA = rnd.Next(1, 7);
+            int DiceB = rnd.Next(1, 7);
+
+            //Display the dice images for the first dice
+            if (DiceA == 1)
+            {
+                //Displays the dice image for the first die
+                DiceOnePictureBoxA.Visible = true;
+            }
+            else if (DiceA == 2)
+            {
+                //Displays the dice image for the second die
+                DiceTwoPictureBoxA.Visible = true;
+            }
+            else if (DiceA == 3)
+            {
+                //Displays the dice image for the third die
+                DiceThreePictureBoxA.Visible = true;
+            }
+            else if (DiceA == 4)
+            {
+                //Displays the dice image for the fourth die
+                DiceFourPictureBoxA.Visible = true;
+            }
+            else if (DiceA == 5)
+            {
+                //Displays the dice image for the fifth die
+                DiceFivePictureBoxA.Visible = true;
+            }
+            else if (DiceA == 6)
+            {
+                //Displays the dice image for the sixth die
+                DiceSixPictureBoxA.Visible = true;
+            }
+
+            //Display the dice images for the second dice
+            if (DiceB == 1)
+            {
+                //Displays the dice image for the first die
+                DiceOnePictureBoxB.Visible = true;
+            }
+            else if (DiceB == 2)
+            {
+                //Displays the dice image for the second die
+                DiceTwoPictureBoxB.Visible = true;
+            }
+            else if (DiceB == 3)
+            {
+                //Displays the dice image for the third die
+                DiceThreePictureBoxB.Visible = true;
+            }
+            else if (DiceB == 4)
+            {
+                //Displays the dice image for the fourth die
+                DiceFourPictureBoxB.Visible = true;
+            }
+            else if (DiceB == 5)
+            {
+                //Displays the dice image for the fifth die
+                DiceFivePictureBoxB.Visible = true;
+            }
+            else if (DiceB == 6)
+            {
+                //Displays the dice image for the sixth die
+                DiceSixPictureBoxB.Visible = true;
+            }
+        }
+
+
+    }
+    }
+
